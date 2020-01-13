0a883403aab5aa2667c2e7e90eae4b8788372ff3
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-13 22:18:50 +0000

    play again switch to new colors

--- script.js
+++ script.js
@@ -58,6 +58,8 @@ resetButton.addEventListener("click", function() {
   colors = generateRandomColors(numSquares);
   pickedColor = pickColor();
   colorDisplay.textContent = pickedColor;
+  messageDisplay.textContent = "";
+  this.textContent = "New Colors";
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
