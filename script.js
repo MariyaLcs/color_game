301e35d1d83f1fe1496d04a73b301c91498bec3e
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-13 19:03:42 +0000

    added reset button

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
 colorDisplay.textContent = pickedColor;
 
@@ -13,6 +15,7 @@ for (var i = 0; i < squares.length; i++) {
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
       changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     } else {
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again";
@@ -46,3 +49,12 @@ function randomColor() {
 
   return "rgb(" + r + ", " + g + ", " + b + ")";
 }
+
+resetButton.addEventListener("click", function() {
+  colors = generateRandomColors(6);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+});
